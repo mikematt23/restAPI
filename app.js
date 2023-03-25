46d800bd10709b9e9ab8ce06d69ba3cb68e209b3
Michael MAtteis <michaelcmatteis@gmail.com>
2023-03-25 12:14:10 -0500

    adding title to index file and rearranging the app js file

--- app.js
+++ app.js
@@ -2,20 +2,17 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 const app = express()
-const db = require('./data/database')
 const users = require('./routes/users')
 const sqlGameRoutes = require('./routes/sqlGameRoutes')
 
-// app.use(express.bodyParser());
 app.use(express.json())
-
+let port = process.env.PORT
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname,'/index.html'))
 })
-
-let port = process.env.PORT
 app.use(sqlGameRoutes)
 app.use(users)
+
 app.listen(port)
 console.log(`app listneing on port ${port}`)
