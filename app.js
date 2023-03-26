2e4d963e9647b5ba0ccbd59e9070f10dc5719f72
Michael MAtteis <michaelcmatteis@gmail.com>
2023-03-26 12:02:15 -0500

    adding bcrypt for the password in users routes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const router = express.Router()
 const path = require('path')
 const app = express()
 const users = require('./routes/users')
@@ -14,5 +13,5 @@ app.get('/',function(req,res){
 app.use(sqlGameRoutes)
 app.use(users)
 
-app.listen(port)
+app.listen(3001)
 console.log(`app listneing on port ${port}`)
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt');
 const db = require('../data/database')
 
 router.get('/users',async function(req,res){
@@ -9,11 +10,14 @@ router.get('/users',async function(req,res){
    return res.json(await db.query(query))
 })
 router.post('/addUser',async (req,res)=>{
-  let userName = req.body.user
-  let password = req.body.password
-  let email = req.body.email
+  const userName = req.body.user
+  const password = req.body.password
+  const email = req.body.email
 
- await db.query(`INSERT INTO users (usersName,password,email,level,lives) VALUES ('${userName}','${password}','${email}',0,0)`)
+  await bcrypt.hash(password, 10, async (err,hash)=>{
+    let hashedPassword = hash
+    await db.query(`INSERT INTO users (usersName,password,email,level,lives) VALUES ('${userName}','${hashedPassword}','${email}',0,0)`)
+  })
  return res.json({message:"it is working"})
 })
 
