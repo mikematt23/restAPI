17cda2b683c96506e9066268ef3e10756e663279
Michael MAtteis <michaelcmatteis@gmail.com>
2023-03-26 12:44:43 -0500

    adding the login funtion

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ router.get('/users',async function(req,res){
    let query = `SELECT * FROM users`
    return res.json(await db.query(query))
 })
+//
 router.post('/addUser',async (req,res)=>{
   const userName = req.body.user
   const password = req.body.password
@@ -20,6 +21,26 @@ router.post('/addUser',async (req,res)=>{
     await db.query(`INSERT INTO users (usersName,password,email,level,lives) VALUES ('${userName}','${hashedPassword}','${email}',0,0)`)
   })
  return res.json({message:"it is working"})
+})
+//
+router.post("/logIn",async (req,res)=>{
+  const userName = req.body.user
+  const password = req.body.password
+  console.log(password)
+  
+  const query = `SELECT * FROM users WHERE usersName = '${userName}'`
+  const user = await db.query(query)
+  
+  console.log(user[0][0].password)
+  await bcrypt.compare(password,user[0][0].password,async (err,result)=>{
+    console.log(result)
+    let isUser = result
+    if(isUser){
+      return res.json(user[0][0])
+    }else{
+      return res.json({message:"password not correct"})
+    }
+  })
 })
 
 module.exports = router
